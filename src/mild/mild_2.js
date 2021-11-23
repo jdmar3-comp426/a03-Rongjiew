31588721a98c5a1d46e33e8b69acc202a1de48aa
Rongjie Wang <rongjiewang@Rongjies-MacBook-Air.local>
2021-11-23 17:08:47 -0500

    1a

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -6,7 +6,7 @@
  * returns: { type: 'number', value: 4 }
  */
 export function identifyVariable(variable) {
-   return {type: typeof(variable), value: valueof(variable)};
+   return {type: typeof(variable), value: variable};
 }
 
 
@@ -48,7 +48,7 @@ export function identifyArray(array) {
  obj now does not contain the `password` field
  */
 export function removeKey(object, key) {
-         delete object.key;
+         delete object[key];
 }
 
 /**
