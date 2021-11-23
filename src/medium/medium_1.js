0dc3b6fbf4667369a4a5c0df66edd3ee44e33437
Rongjie Wang <rongjiewang@Rongjies-MacBook-Air.local>
2021-11-23 16:02:41 -0500

    1a

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -9,7 +9,7 @@ import {variance} from "./data/stats_helpers.js";
  */
 export function getSum(array) {
     let result = 0;
-    for(let i  = 0; i < array.length();i++){
+    for(let i  = 0; i < array.length;i++){
         result += array[i];
     }
     return result;
